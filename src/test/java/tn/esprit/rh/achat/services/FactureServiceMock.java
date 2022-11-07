0034fcb6eacd8b18c082f855e20e020dabe308d7
SuperBatata <khaled.battiche@esprit.tn>
2022-11-07 23:25:48 +0100

    Added mockitos

---
+++ src/test/java/tn/esprit/rh/achat/services/FactureServiceMock.java
@@ -0,0 +1,51 @@
+package tn.esprit.rh.achat.services;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.repositories.FactureRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class FactureServiceMock {
+    @Autowired
+    FactureRepository fr;
+    FactureRepository factureRepository = Mockito.mock(FactureRepository.class);
+
+    Facture facture;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        facture = new Facture();
+        facture.setMontantRemise(50);
+        facture.setMontantFacture(101);
+        facture.setDateDerniereModificationFacture(null);
+        facture.setDateCreationFacture(new java.util.Date());
+
+        factureRepository.save(facture);
+    }
+
+    List<Facture> factures =  new ArrayList<>();
+
+
+    @Test
+    public void testRetrieveAllFactures() {
+        Mockito.when(factureRepository.findById(1L)).thenReturn(java.util.Optional.of(facture));
+        float factureFound = facture.getMontantFacture();
+        Assertions.assertNotNull(factureFound);
+    }
+
+}
--- src/test/java/tn/esprit/rh/achat/services/FactureServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/FactureServiceTest.java
@@ -1,6 +1,8 @@
 package tn.esprit.rh.achat.services;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Facture;
